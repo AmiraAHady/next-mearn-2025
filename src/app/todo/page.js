f34e55a4a4aab6781aff4cb4dfc6a7339740ce40
AmiraAHady <amirasgithub@gmail.com>
2025-05-23 12:36:04 +0300

    add mongo pass

--- src/app/todo/page.js
+++ src/app/todo/page.js
@@ -4,7 +4,7 @@ import { GET } from '../api/todo/route'
 export default async function ToDoList() {
 const res=await GET()
 const {data:todos}=await res.json()
-console.log(todos);
+
   return (
     <>
     {todos.map((todoItem)=>{
